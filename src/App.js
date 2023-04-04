1fe927f83783f19138e526937d39d33a6002d2d6
Aditya Sharma <aditya.sharma.adbu@gmail.com>
2023-04-04 11:21:52 +0530

    RKT tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/store";
 
 function App() {
+
+  const counter = useSelector((state)=>state.counter)
+  const dispatch = useDispatch();
+
+  const increment=()=>{
+      dispatch(actions.increment())
+  }
+
+  const decrement=()=>{
+    dispatch(actions.decrement())
+  }
+
   return (
-    <div className="App">
-      
+    <div className="App" style={{textAlign:"center", padding:"10%"}}>
+      <h1>Counter App</h1>
+      <h4>{counter}</h4>
+      <button className="btn btn-success " onClick={increment}>Increment</button>
+      <button className="btn btn-danger" onClick={decrement}>Decrement</button>
     </div>
   );
 }
